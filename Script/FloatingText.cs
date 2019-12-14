a1713acf8596635dfbd3c78fba23b775e76582f2
kimjongbuem <42004058+kimjongbuem@users.noreply.github.com>
2019-12-14 23:08:36 +0900

    Add files via upload

---
+++ Script/FloatingText.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class FloatingText : MonoBehaviour
+{
+    public Text text;
+    public float moveSpeed; public float destoryTime;
+    private Vector3 vec;
+    // Update is called once per frame
+    void Update()
+    {
+        vec.Set(text.transform.position.x, text.transform.position.y + (moveSpeed * Time.deltaTime), text.transform.position.z);
+        text.transform.position = vec;
+        destoryTime -= Time.deltaTime;
+        if (destoryTime <= 0) Destroy(this.gameObject);
+
+    }
+}
